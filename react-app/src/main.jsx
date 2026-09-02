655df96bc90cf251b3ea6e401fbb9037c0d68cbd
gustomotaz-star <gustomotaz@gmail.com>
2026-09-02 13:37:37 +0300

    Load centralized content page styles

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -8,6 +8,7 @@ import { I18nProvider } from './context/I18nContext';
 import './styles/global.css';
 import './styles/storefront.css';
 import './styles/admin.css';
+import './styles/content.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
