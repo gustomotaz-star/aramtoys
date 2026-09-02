d33bf878c6fe69a1b53ae91ec589c645c4432092
gustomotaz-star <gustomotaz@gmail.com>
2026-09-02 04:56:36 +0300

    Wire React app providers

---
+++ react-app/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import { I18nProvider } from './context/I18nContext';
+import './styles/global.css';
+import './styles/storefront.css';
+import './styles/admin.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <I18nProvider>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
+      </I18nProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
